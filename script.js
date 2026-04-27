6b9b36f2dc51388e6d5b0453f0d395d4cadb6f9e
Dinara <dinara.mentos@mail.ru>
2026-04-27 09:54:30 +0300

    Add script.js and update index.html

---
+++ script.js
@@ -0,0 +1,15 @@
+const num = Number(prompt());
+
+function numBuzz(num) {
+    if(num % 3 === 0 && num % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if(num % 3 === 0) {
+        console.log("Fizz");
+    } else if(num % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log("Ничего");
+    }
+}
+
+numBuzz(num);
